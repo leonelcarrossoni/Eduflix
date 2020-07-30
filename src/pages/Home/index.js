2b6a20f4a211f507334eba7b9b067953dbfd58cf
Leonel <leonel.carrossoni@gmail.com>
2020-07-30 09:56:56 -0300

    Adicionando conteúdos da página

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import Menu from '../../components/Menu'
+import dados_iniciais from '../../data/dados_iniciais.json'
+import BannerMain from '../../components/BannerMain'
+import Carousel from '../../components/Carousel'
+import Footer from '../../components/Footer'
 
-import Menu from './components/Menu'
-import dados_iniciais from './data/dados_iniciais.json'
-import BannerMain from './components/BannerMain'
-import Carousel from './components/Carousel'
-import Footer from './components/Footer'
-
-function App() {
+function Home() {
   return (
     <div>
       <Menu />
@@ -36,4 +35,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
