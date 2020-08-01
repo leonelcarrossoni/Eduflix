5a5406031bf16a9ad81a94109df29ab4cc7f16ee
Leonel <leonel.carrossoni@gmail.com>
2020-08-01 15:22:08 -0300

    Atualizando categorias

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -30,8 +30,14 @@ function CadastroCategoria() {
 
   useEffect(() => {
     console.log('teste');
-    const URL_TOP ='http://localhost:8080/categorias'
-    fetch('URL_TOP');
+    const URL_TOP = 'http://localhost:8080/categorias';
+    fetch(URL_TOP)
+      .then(async (respostaDoServidor) => {
+        const resposta = await respostaDoServidor.json();
+        setCategorias([
+          ...resposta,
+        ]);
+      });
 
   //   setTimeout(() => {
   //     setCategorias([
@@ -51,7 +57,7 @@ function CadastroCategoria() {
   //     ]);
   //   }, 1 * 1000);
   // }, []);
-   })
+  });
 
   return (
     <PageDefault>
